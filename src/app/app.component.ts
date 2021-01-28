8eaab9c9565bfcc647c8ffc5b51e1d5faf8d92b8
Ellis Goranov <me@ellis.pw>
2021-01-28 22:22:15 +0000

    add design for chatbox

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component, ElementRef, ViewChild } from '@angular/core';
-import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Form, FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { GameService } from './_services';
 
 @Component({
@@ -14,12 +14,17 @@ export class AppComponent {
   gameId!: string | null;
 
   form: FormGroup
+  chatBoxForm: FormGroup
 
   constructor(public gameService: GameService, private forms: FormBuilder) {
     gameService.GameId.subscribe(result => this.gameId = result);
     
     this.form = forms.group({
       gameId: ['', Validators.required]
+    })
+
+    this.chatBoxForm = forms.group({
+      input: ['', Validators.required]
     })
 
     gameService.GameEvents.subscribe(messageObject => {
