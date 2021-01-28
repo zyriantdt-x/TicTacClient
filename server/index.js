bf7a691339963bbce744bda59a69fab2ea386b8b
Ellis Goranov <me@ellis.pw>
2021-01-28 20:02:44 +0000

    fix typo

--- server/index.js
+++ server/index.js
@@ -5,7 +5,7 @@ let app = express();
 app.use(express.static("../dist"));
 
 app.get("/*", (req, res, next) => {
-    return res.sendFile(__dirname + "../dist/index.html");
+    return res.sendFile(__dirname + "/../dist/index.html");
 });
 
 app.listen(80);
