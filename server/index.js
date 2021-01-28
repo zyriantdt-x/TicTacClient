fe5a9972451411269ce50943ae754c09a446b938
Ellis Goranov <me@ellis.pw>
2021-01-28 20:04:32 +0000

    add path resolve

--- server/index.js
+++ server/index.js
@@ -1,11 +1,12 @@
 const express = require("express");
+const path = require("path");
 
 let app = express();
 
 app.use(express.static("../dist"));
 
 app.get("/*", (req, res, next) => {
-    return res.sendFile(__dirname + "/../dist/index.html");
+    return res.sendFile(path.resolve(__dirname + "/../dist/index.html"));
 });
 
 app.listen(80);
